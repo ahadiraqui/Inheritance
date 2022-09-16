938d68ef7d3955b6a54089fc1f2d137eb6ae4142
ahadiraqui <66622782+ahadiraqui@users.noreply.github.com>
2022-09-16 22:35:50 +0530

    Create shape.js

---
+++ shape.js
@@ -0,0 +1,15 @@
+class Shape
+{
+    drawShape(name)
+    {
+        return name;
+    }
+    calculateArea()
+    {
+        return 0;
+    }
+    color(color)
+    {
+        return color;
+    }
+ }
