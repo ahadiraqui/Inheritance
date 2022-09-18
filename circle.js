7a995b357655323804366fc721927be624247f04
ahadiraqui <66622782+ahadiraqui@users.noreply.github.com>
2022-09-18 13:30:13 +0530

    Update circle.js

--- circle.js
+++ circle.js
@@ -1,11 +1,11 @@
-class Circle extends Shape {                    // The "extends" keyword is what we use to tell java that Circle inherits the functionality of Shape.
-
-    static final double PI = Math.PI;   // constant
-    double diameter;                    // This could be any number, representing the diameter of this circle.
-
-    public double  calculateArea () {
-        double radius = diameter / 2.0;
-        return PI * radius * radius;
+class Circle extends Shape
+{
+    calculateArea(diameter)
+    {
+        const pi= Math.PI;
+        const radius=diameter/2;
+        return (pi*radius*radius);
+       
     }
 
 }
